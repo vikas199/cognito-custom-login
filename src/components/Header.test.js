09f0518cb2a0a63bdd4e519120cf34a46f988cc5
nasir2712 <nasir.mohd2712@gmail.com>
2018-07-30 13:50:19 -0700

    add logo header test case

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Header from './Header'
+import headerLogo from './../assets/images/CWS-CARES-tempLogo.png'
 
 describe('ErrorMessage.js Tests', () => {
-  // it('should display correct image', () => {
-  //   const wrapper = shallow(<Header/>)
+  it('should display correct image', () => {
+    const wrapper = shallow(<Header/>)
 
-  //   let img = wrapper.find('img')
-  //   console.log(wrapper.debug())
-  //   expect(img).toHaveLength(1)
-  //   expect(img.at(0).props().src).toEqual('')
-  // })
+    let img = wrapper.find('img')
+
+    expect(img).toHaveLength(1)
+    // console.log(img.at(0).props().src)
+    expect(img.at(0).props().src).toEqual(headerLogo)
+  })
 
   it('should center image', () => {
     const wrapper = shallow(<Header/>)
