21c29f4ed78b873634e45dc2e6aca2044b12a5a5
nasir2712 <nasir.mohd2712@gmail.com>
2018-08-03 09:58:50 -0700

    removing exact and base path route

--- src/components/App.js
+++ src/components/App.js
@@ -23,9 +23,8 @@ class App extends React.Component {
                 <div id="div-forms" className="modal-body">
                   <BrowserRouter>
                     <Switch>
-                      <Route exact path="/" component={LoginPage} />
-                      <Route exact path="/login" component={LoginPage} />
-                      <Route exact path="/forgotpassword" component={ForgotPasswordPage} />
+                      <Route path="/login" component={LoginPage} />
+                      <Route path="/forgotpassword" component={ForgotPasswordPage} />
                     </Switch>
                   </BrowserRouter>
                 </div>
